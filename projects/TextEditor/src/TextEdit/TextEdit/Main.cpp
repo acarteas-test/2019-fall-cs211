0904dcbb15f45cfb3dd99839df5e641295fb54ed
Christopher Hilton <cphiltonjr@gmail.com>
2019-09-10 10:25:22 -0700

    adding milestone 1 code

--- projects/TextEditor/src/TextEdit/TextEdit/Main.cpp
+++ projects/TextEditor/src/TextEdit/TextEdit/Main.cpp
@@ -3,6 +3,12 @@
 #include "curses.h"
 #include <stdlib.h>
 #include <time.h>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+void PrintTabs(const char* tabs[], WINDOW* winptr);
 
 int main(void)
 {
@@ -30,7 +36,25 @@ int main(void)
 	curs_set(FALSE);
 
 	//MAIN PROGRAM LOGIC GOES HERE
+	
+	//Turning color 
+	if (has_colors() == FALSE)
+	{
+		endwin();
+		printf("The Terminal does not support color");
+		exit(EXIT_FAILURE);
+	}
+	start_color();
 
+	//Picking color for columns
+	init_pair(1, COLOR_BLUE, COLOR_WHITE);
+	attron(COLOR_PAIR(1));
+
+	//Setting up boarders
+	mvaddch(1, 2, ACS_BSSB);						//inner corner boarders
+	mvaddch(1, num_cols - 3, ACS_BBSS);  
+	mvaddch(num_rows - 2, num_cols - 3, ACS_SBBS);
+	mvaddch(num_rows - 2, 3,ACS_BBSS);
 	for (int i = 0; i < num_cols; i++)
 	{
 		//top row
@@ -43,12 +67,39 @@ int main(void)
 	for (int i = 0; i < num_rows; i++)
 	{
 		//left column
-		mvaddch(i, 0, ACS_BBSS);
+		mvaddch(i+1, 0, ACS_BLOCK);
+		mvaddch(i+1, 1, ACS_BLOCK);
 
 		//right column
-		mvaddch(i, num_cols - 1, ACS_BLOCK);
+		mvaddch(i+1, num_cols - 1, ACS_BLOCK);
+		mvaddch(i + 1, num_cols - 2, ACS_BLOCK);
+
 	}
 
+	//turring off color for borders
+	attroff(COLOR_PAIR(1));
+	
+	//coloring text
+	init_pair(2, COLOR_CYAN, COLOR_BLACK);
+	attron(COLOR_PAIR(2));
+	attron(A_BOLD | A_BLINK);
+
+	//Text windows
+	string file = "File";				//string declaration
+	string edit = "Edit";
+	string options = "Options";
+	string tools = "Tools";
+	string help = "Help";
+	const char* filechar = file.c_str(); //conversion into const char* so mvprintw works
+	const char* editchar = edit.c_str();
+	const char* optionschar = options.c_str();
+	const char* toolschar = tools.c_str();
+	const char* helpchar = help.c_str();
+	
+	static const char* tabs[5] = {filechar, editchar, optionschar, toolschar, helpchar};
+
+	PrintTabs(tabs, main_window);
+	
 	//refresh tells curses to draw everything
 
 	//END OF PROGRAM LOGIC GOES HERE
@@ -58,4 +109,25 @@ int main(void)
 
 	//end curses mode
 	endwin();
+}
+
+//is suppose to make my life easier by printing the top tabs (but its more work than its worth)
+void PrintTabs(const char* tabs[], WINDOW* winptr)
+{
+	//Color for the text
+	init_pair(2, COLOR_CYAN, COLOR_BLACK);
+	attron(COLOR_PAIR(2));
+	attron(A_BOLD | A_BLINK);
+
+	int pos = 3;
+	for (int i = 0; i < 5; i++)
+	{
+		if(i == 0)
+		mvwprintw(winptr, 0, 2, "|");
+		mvwprintw(winptr, 0, pos, tabs[i]);
+		printw(" | ");
+		pos = pos + strlen(tabs[i]) + 3;
+	}
+	attroff(COLOR_PAIR(2));
+	attroff(A_BOLD | A_BLINK);
 }
